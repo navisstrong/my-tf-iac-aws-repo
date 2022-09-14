1fa8ad653b50f5350c6a6efe46d3456c4b82cd1d
navisstrong <75235702+navisstrong@users.noreply.github.com>
2022-09-14 13:18:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,54 +2,61 @@ provider "aws" {
   region = var.aws_region
 }
 
-#Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
+resource "aws_security_group" "mysecuritygroup" {
+  name        = "var.security_group"
+  description = "Security group for EC2 instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "TLS from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
- # outbound from jenkis server
   egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  tags= {
-    Name = var.security_group
+  tags = {
+    Name = "var.security_group"
   }
 }
 
-# Create AWS ec2 instance
 resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
-  key_name = var.key_name
   instance_type = var.instance_type
-  security_groups= [var.security_group]
-  tags= {
+  key_name = var.key_name
+  security_groups = [var.security_group]
+
+  tags = {
     Name = var.tag_name
   }
 }
 
-# Create Elastic IP address
 resource "aws_eip" "myFirstInstance" {
-  vpc      = true
   instance = aws_instance.myFirstInstance.id
-tags= {
-    Name = "my_elastic_ip"
+  vpc      = true
+  tags= {
+    Name = "myelasticIP"
   }
 }
